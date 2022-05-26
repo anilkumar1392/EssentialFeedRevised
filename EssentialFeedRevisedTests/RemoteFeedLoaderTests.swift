cbe4aa8f0f12b2a15bc3727926bc3f67840a54a6
Anil kumar <anil1.kumar@africa.airtel.com>
2022-05-26 13:04:59 +0530

    request data from URL on 'load'

--- EssentialFeedRevisedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedRevisedTests/RemoteFeedLoaderTests.swift
@@ -9,19 +9,33 @@ import Foundation
 import XCTest
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    private init() {}
+    
     var requestedURL: URL?
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFormURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         let _ = RemoteFeedLoader()
                 
         XCTAssertNil(client.requestedURL, "Expected to return nil")
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }
