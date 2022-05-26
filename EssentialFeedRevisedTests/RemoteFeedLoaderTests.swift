5fb0162c000a7adfb32d125cb4a5094287d30ff0
Anil kumar <anil1.kumar@africa.airtel.com>
2022-05-26 17:13:47 +0530

    add more test assertion for different  https code

--- EssentialFeedRevisedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedRevisedTests/RemoteFeedLoaderTests.swift
@@ -93,14 +93,15 @@ class RemoteFeedLoaderTests: XCTestCase {
     // Invalid data error course
     func test_load_deliverErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
-        
-        var capturedErrors = [RemoteFeedLoader.Error]()
-        sut.load { capturedErrors.append($0) }
-        
-        let errorCode = 400
-        client.complete(with: errorCode)
-        
-        XCTAssertEqual(capturedErrors, [.invalidData])
+
+        let samples = [199, 210, 300, 400 , 500]
+        samples.enumerated().forEach { (index, code) in
+            var capturedErrors = [RemoteFeedLoader.Error]()
+            sut.load { capturedErrors.append($0) }
+            client.complete(with: code, at: index)
+            
+            XCTAssertEqual(capturedErrors, [.invalidData])
+        }
     }
 }
 
