e2b604050bdc99292efc3e51ff7bc06172844d72
Anil kumar <anil1.kumar@africa.airtel.com>
2022-05-26 16:26:25 +0530

    Gurarantee we only receive on error

--- EssentialFeedRevisedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedRevisedTests/RemoteFeedLoaderTests.swift
@@ -78,13 +78,11 @@ class RemoteFeedLoaderTests: XCTestCase {
         let (sut, client) = makeSUT()
         client.error = NSError(domain: "Test", code: 0)
         
-        var capturedError: RemoteFeedLoader.Error?
+        var capturedErrors = [RemoteFeedLoader.Error]()
         
-        sut.load { error in
-            capturedError = error
-        }
+        sut.load { capturedError.append($0) }
         
-        XCTAssertEqual(capturedError, .connectivity)
+        XCTAssertEqual(capturedErrors, [.connectivity])
     }
 }
 
