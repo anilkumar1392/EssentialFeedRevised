142d41a08ff7b6c66a5396ebd92e3c377639d941
Anil kumar <anil1.kumar@africa.airtel.com>
2022-05-26 16:22:21 +0530

    Delivers connectivity error on client error

--- EssentialFeedRevisedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedRevisedTests/RemoteFeedLoaderTests.swift
@@ -10,7 +10,7 @@ import XCTest
 import EssentialFeedRevised
 
 /*
- Tecture 1.
+ Lecture 1.
  From Singletons and Globals to Proper Dependency Injection
  
  Learning Outcomes
@@ -22,6 +22,24 @@ import EssentialFeedRevised
  Controlling your dependencies: Locating globally shared instances (Implicit) vs. Injecting dependencies (Explicit)
  Controlling your dependencies: Dependency injection
  
+ */
+
+/*
+ Lecture 2.
+ Asserting a Captured Value Is Not Enough + Cross-Module Access Control
+ 
+ Learning Outcomes
+ Understand the trade-offs of access control for testing purposes
+ Expand behavior checking (and coverage) using test spy objects
+ 
+ Test visibility: public vs @testable (internal)
+ We start by moving the RemoteFeedLoader class and the HTTPClient protocol to a new production file to facilitate our development workflow. By doing so, our test target doesn’t have access to our internal types anymore; thus we need to decide whether we will make our types public or leave them as internal and add the @testable attribute when importing our production module in the tests. We decided the former as we can test the Feed API module through the public interfaces it contains so that we can test the expected behavior as a client of the module. Another benefit of this approach is that we can now make changes to any internal or private implementation details without breaking our tests.
+ 
+ */
+
+/*
+ Lecture 3.
+ Handling Errors + Stubbing vs. Spying + Eliminating Invalid Paths
  */
 
 class RemoteFeedLoaderTests: XCTestCase {
@@ -39,7 +57,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         
-        sut.load()
+        sut.load { _ in }
         
         XCTAssertEqual(client.requestedURLs, [url])
     }
@@ -50,10 +68,23 @@ class RemoteFeedLoaderTests: XCTestCase {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         
-        sut.load()
-        sut.load()
+        sut.load { _ in }
+        sut.load { _ in }
         
         XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        client.error = NSError(domain: "Test", code: 0)
+        
+        var capturedError: RemoteFeedLoader.Error?
+        
+        sut.load { error in
+            capturedError = error
+        }
+        
+        XCTAssertEqual(capturedError, .connectivity)
     }
 }
 
@@ -67,10 +98,16 @@ extension RemoteFeedLoaderTests {
     }
     
     private class HTTPClientSpy: HTTPClient {
+        
         var requestedURLs = [URL]()
+        var error: Error?
 
-        func get(from url: URL) {
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+            if let error = error {
+                completion(error)
+            }
             requestedURLs.append(url)
         }
+
     }
 }
