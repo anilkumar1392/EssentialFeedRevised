8ff0af8b8dc90b686769d91bbc191b5d61b225d5
Anil kumar <anilpal1392@gmail.com>
2022-06-03 10:26:27 +0530

    Simplify type declaration with a helper function

--- EssentialFeedRevisedTests/Feed Api/RemoteFeedLoaderTests.swift
+++ EssentialFeedRevisedTests/Feed Api/RemoteFeedLoaderTests.swift
@@ -103,7 +103,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
         
-        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.connectivity)) {
+        expect(sut, toCompleteWith: failure(.connectivity)) {
             let clientError = NSError(domain: "Test", code: 0)
             client.complete(with: clientError, at: 0)
         }
@@ -115,7 +115,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 
         let samples = [199, 210, 300, 400 , 500]
         samples.enumerated().forEach { (index, code) in
-            expect( sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData)) {
+            expect( sut, toCompleteWith: failure(.invalidData)) {
                 let json = makeItemJson([])
                 client.complete(with: code, data: json, at: index)
             } 
@@ -127,7 +127,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOn200HttpsReponseWithInvalidJson() {
         let (sut, client) = makeSUT()
         
-        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData)) {
+        expect(sut, toCompleteWith: failure(.invalidData)) {
             let invalidJson = Data("Invalid json".utf8)
             client.complete(with: 200, data: invalidJson, at: 0)
         }
@@ -237,6 +237,9 @@ extension RemoteFeedLoaderTests {
         return (item, json)
     }
     
+    private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+        return .failure(error)
+    }
     private func makeItemJson(_ items: [[String: Any]]) -> Data {
         let json = ["items": items]
         return try! JSONSerialization.data(withJSONObject: json)
