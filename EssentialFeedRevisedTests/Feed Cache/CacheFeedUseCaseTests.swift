0d5016d926b9b3c2126cf423031aeec8425eaf8c
Anil kumar <anilpal1392@gmail.com>
2022-06-15 06:48:32 +0530

    Save command request cache deletion

--- EssentialFeedRevisedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedRevisedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,9 +6,14 @@
 //
 
 import XCTest
+import EssentialFeedRevised
 
 class FeedStore {
     var deleteCachedFeedcallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedcallCount += 1
+    }
 }
 
 class LocalFeedLoader {
@@ -17,13 +22,43 @@ class LocalFeedLoader {
     init(store: FeedStore) {
         self.store = store
     }
+    
+    func save(_ items: [FeedImage]) {
+        store.deleteCachedFeed()
+    }
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
     func test_init_doesNotDeleteCacheUponCreation() {
         let store = FeedStore()
-        let sut = LocalFeedLoader(store: store)
+        _ = LocalFeedLoader(store: store)
         
         XCTAssertEqual(store.deleteCachedFeedcallCount, 0)
     }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedFeedcallCount, 1)
+    }
+}
+
+// MARK: - Helepr methods
+
+extension CacheFeedUseCaseTests {
+    func makeSUT(file: StaticString = #file, line: UInt = #line) {
+    }
+    
+    private func uniqueItem() -> FeedImage {
+        return FeedImage(id: UUID(), description: "Any description", location: "A location", imageURL: anyURL())
+    }
+    
+    func anyURL() -> URL {
+        return URL(string: "Http://any-url.com")!
+    }
+
 }
