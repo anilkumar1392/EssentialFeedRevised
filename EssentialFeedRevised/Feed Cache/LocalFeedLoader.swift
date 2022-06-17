4ec91878dbcc58186fb5a2034955075c03b6a993
Anil kumar <anilpal1392@gmail.com>
2022-06-17 09:01:40 +0530

    Make 'LocalFeedLoader' conform to FeedLoader protocol

--- EssentialFeedRevised/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeedRevised/Feed Cache/LocalFeedLoader.swift
@@ -57,7 +57,7 @@ extension LocalFeedLoader {
 
 // MARK: - Load command
 
-extension LocalFeedLoader {
+extension LocalFeedLoader: FeedLoader {
     public func load(completion: @escaping (LoadResult) -> Void) {
         store.retrieve { [weak self] result in
             guard let self = self else { return }
