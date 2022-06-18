f3e421f067c84079de0b72dc5cb6a8381d7bb647
Anil kumar <anilpal1392@gmail.com>
2022-06-18 15:00:05 +0530

    make 'CodableFeedStore' queue concurrent to allow multiple 'retrieve' to be processed in parallel (Since it has no side effects) and use barrier when performing side effects to guarantee data consistency and avoid race conditions.

--- EssentialFeedRevised/Feed Cache/CodableFeedStore.swift
+++ EssentialFeedRevised/Feed Cache/CodableFeedStore.swift
@@ -41,7 +41,7 @@ public class CodableFeedStore: FeedStore {
         self.storeURL = storeURL
     }
     
-    private let queue = DispatchQueue(label: "\(CodableFeedStore.self)Queue", qos: .userInitiated)
+    private let queue = DispatchQueue(label: "\(CodableFeedStore.self)Queue", qos: .userInitiated, attributes: .concurrent)
     
     public func retrieve(completion: @escaping RetrivalCompletion) {
         let storeURL = self.storeURL
@@ -61,7 +61,7 @@ public class CodableFeedStore: FeedStore {
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         let storeURL = self.storeURL
-        queue.async {
+        queue.async(flags: .barrier) {
             do {
                 let encoder = JSONEncoder()
                 let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
@@ -76,7 +76,7 @@ public class CodableFeedStore: FeedStore {
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         let storeURL = self.storeURL
-        queue.async {
+        queue.async(flags: .barrier) {
             guard FileManager.default.fileExists(atPath: storeURL.path) else {
                 return completion(nil)
             }
--- EssentialFeedRevised/Ref For Persistance Module/Persistance_lecture_13.md
+++ EssentialFeedRevised/Ref For Persistance Module/Persistance_lecture_13.md
@@ -1,3 +1,5 @@
 
 ## Designing and Testing Thread-safe Components with DispatchQueue, Serial vs. Concurrent Queues, Thread-safe Value Types, and Avoiding Race Conditions
 
+// Least side effect you have the more concurrent your app can be.
+side effects are eneny of concurrency.
