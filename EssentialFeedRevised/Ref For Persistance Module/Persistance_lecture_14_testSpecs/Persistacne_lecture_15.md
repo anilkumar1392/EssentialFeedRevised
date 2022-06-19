a042661fcef8a46564a7b198cd3ae1e9253d8da9
Anil kumar <anilpal1392@gmail.com>
2022-06-19 06:44:25 +0530

    Add empty core data feedStore specs

---
+++ EssentialFeedRevised/Ref For Persistance Module/Persistance_lecture_14_testSpecs/Persistacne_lecture_15.md
@@ -0,0 +1,2 @@
+## Core Data Overview, Implementation, Concurrency Model, Trade-offs, Modeling & Testing Techniques, and Implementing Reusable Protocol Specs
+
---
+++ EssentialFeedRevisedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,84 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentialFeedRevisedTests
+//
+//  Created by 13401027 on 19/06/22.
+//
+
+import Foundation
+import XCTest
+
+class CoreDataFeedStoreTests: XCTestCase, FailableFeedStore {
+    func test_retrieve_deliversFailureOnRetrivalError() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnFailure() {
+        
+    }
+    
+    func test_insert_deliversErrorOnInsertionError() {
+        
+    }
+    
+    func test_insert_hasNoSideEffectOnInsertionError() {
+        
+    }
+    
+    func test_delete_deliverErrorOnDeletionError() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectsOnDeletionError() {
+        
+    }
+    
+    func test_retrieve_deliverEmptyOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCachedValues() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectOnEmptyCache() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+        
+    }
+    
+    func test_storeSideEffects_runSerially() {
+        
+    }
+    
+}
