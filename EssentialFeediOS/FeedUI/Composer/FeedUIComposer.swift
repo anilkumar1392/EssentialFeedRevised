5fa6858516f9d3cb104861a0ba1f45fccc8373c1
Anil kumar <anilpal1392@gmail.com>
2022-07-08 10:41:40 +0530

    Move FeedViewAdapter to separate file

--- EssentialFeediOS/FeedUI/Composer/FeedUIComposer.swift
+++ EssentialFeediOS/FeedUI/Composer/FeedUIComposer.swift
@@ -99,31 +99,5 @@ final class FeedViewAdapter: FeedView {
     }
 } */
 
-final class FeedViewAdapter: FeedView {
-    private weak var controller: FeedViewController?
-    private let imageLoader: FeedImageDataLoader
-    
-    init(controller: FeedViewController, imageLoader: FeedImageDataLoader) {
-        self.controller = controller
-        self.imageLoader = imageLoader
-    }
-    
-    // Here FeedImageDataLoaderAdapter is an adapter
-    // 1. it has ref to a presenter and some load image api call.
-    
-    func display(_ viewModel: FeedViewModel) {
-        controller?.tableModel = viewModel.feed.map { model in
-            let adapter = FeedImageDataLoaderPresentationAdapter<WeakRefVirtualProxy<FeedImageCellController>, UIImage>(model: model, imageLoader: imageLoader)
-            let view = FeedImageCellController(delegate: adapter)
-
-            adapter.presenter = FeedImagePresenter(
-                view: WeakRefVirtualProxy(view),
-                imageTransformer: UIImage.init)
-
-            return view
-        }
-    }
-    
-}
 
 
