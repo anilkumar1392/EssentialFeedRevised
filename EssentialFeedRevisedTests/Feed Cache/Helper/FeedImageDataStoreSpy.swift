6c154bb72614cda0a380864ba4c7fbd97679cddf
Anil kumar <anilpal1392@gmail.com>
2022-07-10 12:43:40 +0530

    Extract FeedImageDataStoreSpy into a shared scope

---
+++ EssentialFeedRevisedTests/Feed Cache/Helper/FeedImageDataStoreSpy.swift
@@ -0,0 +1,42 @@
+//
+//  FeedImageDataStoreSpy.swift
+//  EssentialFeedRevisedTests
+//
+//  Created by 13401027 on 10/07/22.
+//
+
+import Foundation
+
+// MARK: - Spy helper
+extension LoadFeedImageDataFromCacheUseCaseTests {
+    class StoreSpy: FeedImageDataStore {
+        enum Message: Equatable {
+            case retrieve(dataFor: URL)
+            case insert(data: Data, forUrl: URL)
+        }
+        
+        private(set) var receivedMessages = [Message]()
+        private var retrievalCompletions = [(FeedImageDataStore.RetrievalResult) -> Void]()
+        private var insertionCompletions = [(InsertionResult) -> Void]()
+
+        func retrieve(dataFromURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+            receivedMessages.append(.retrieve(dataFor: url))
+            retrievalCompletions.append(completion)
+        }
+        
+        func completeRetrieval(with error: Error, at index: Int = 0) {
+            retrievalCompletions[index](.failure(error))
+        }
+        
+        func completeRetrieval(with data: Data?, at index: Int = 0) {
+            retrievalCompletions[index](.success(data))
+        }
+        
+        // MARK: - Insertion
+        
+        func insert(_ data: Data, forUrl url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+            receivedMessages.append(.insert(data: data, forUrl: url))
+            insertionCompletions.append(completion)
+        }
+    }
+}
--- EssentialFeedRevisedTests/Feed Cache/LoadFeedImageDataFromCacheUseCaseTests.swift
+++ EssentialFeedRevisedTests/Feed Cache/LoadFeedImageDataFromCacheUseCaseTests.swift
@@ -165,39 +165,6 @@ extension LoadFeedImageDataFromCacheUseCaseTests {
     }
 }
 
-// MARK: - Spy helper
-extension LoadFeedImageDataFromCacheUseCaseTests {
-    private class StoreSpy: FeedImageDataStore {
-        enum Message: Equatable {
-            case retrieve(dataFor: URL)
-            case insert(data: Data, forUrl: URL)
-        }
-        
-        private(set) var receivedMessages = [Message]()
-        private var retrievalCompletions = [(FeedImageDataStore.RetrievalResult) -> Void]()
-        private var insertionCompletions = [(InsertionResult) -> Void]()
-
-        func retrieve(dataFromURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
-            receivedMessages.append(.retrieve(dataFor: url))
-            retrievalCompletions.append(completion)
-        }
-        
-        func completeRetrieval(with error: Error, at index: Int = 0) {
-            retrievalCompletions[index](.failure(error))
-        }
-        
-        func completeRetrieval(with data: Data?, at index: Int = 0) {
-            retrievalCompletions[index](.success(data))
-        }
-        
-        // MARK: - Insertion
-        
-        func insert(_ data: Data, forUrl url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
-            receivedMessages.append(.insert(data: data, forUrl: url))
-            insertionCompletions.append(completion)
-        }
-    }
-}
 
 /*
  ---
